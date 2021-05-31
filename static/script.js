d449ec86ee741dc6f7de9392d64b818c3a398a11
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2021-05-31 18:53:44 +0100

    layout of page

--- static/script.js
+++ static/script.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+// document.addEventListener('scroll',function(){
+//     const pageScroll = 0
+//     document.getElementById('wave01').style.transform = `translateY(-${pageScroll}px)`;
+//     document.getElementById('wave02').style.transform = `translateY(-${pageScroll}px)`;
+//     document.getElementById('wave03').style.transform = `translateY(-${pageScroll}px)`;
+//     document.getElementById('wave04').style.transform = `translateY(-${pageScroll}px)`;
+//     document.getElementById('wave05').style.transform = `translateY(-${pageScroll}px)`;
+//     document.getElementById('wave06').style.transform = `translateY(-${pageScroll}px)`;
+// })
